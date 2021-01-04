f4c8385947083e0ac616642dd03f46c3327930b7
Neil Byrne <G00343624@gmit.ie>
2021-01-04 14:34:00 +0000

    Predetermined paths for 2 birds and 1 bee added

---
+++ Birds and Bees Simulation/Assets/Scripts/EnvironmentConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentConfig : MonoBehaviour
+{
+    public Flower flower0;
+    public Flower flower1;
+    public Flower flower2;
+    public Flower flower3;
+    public Flower flower4;
+    public Flower flower5;
+    public Flower flower6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
